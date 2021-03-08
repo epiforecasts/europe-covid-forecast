4b98b8b0fac1ada84b0177b5c7d2b4eeb0544a01
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-08 22:48:52 +0000

    add submission

--- rt-forecast/submission.R
+++ rt-forecast/submission.R
@@ -15,7 +15,7 @@ case_forecast <- suppressWarnings(
     date = ymd(target_date), forecast = TRUE,
     samples = TRUE)$estimated_reported_cases$samples)
 
-death_from_cases_forecast <- fread(
+death_forecast <- fread(
   here("rt-forecast", "data", "samples", "deaths",
        target_date, "samples.csv"))
 
@@ -29,23 +29,23 @@ locations <- fread(paste0(
 # Format forecasts --------------------------------------------------------
 case_forecast <- format_forecast(case_forecast[, value := cases],
                                  locations = locations,
-                                 cumulative =  cum_cases,
                                  forecast_date = target_date,
                                  submission_date = target_date,
                                  CrI_samples = 0.9,
                                  target_value = "case")
 
-death_forecast <- format_forecast(death_from_cases_forecast,
-                                             locations = locations,
-                                             cumulative = cum_deaths,
-                                             forecast_date = target_date,
-                                             submission_date = target_date,
-                                             target_value = "death")
+death_forecast <- format_forecast(death_forecast,
+                                  locations = locations,
+                                  forecast_date = target_date,
+                                  submission_date = target_date,
+                                  target_value = "death")
 
-forecast <- rbindlist(list(case_forecast, death_forecast))
+death_forecast[, target_end_date := as.character(target_end_date)]
+case_forecast[, target_end_date := as.character(target_end_date)]
+forecast <- rbind(case_forecast, death_forecast, use.names = TRUE)
 
 # Save forecasts ----------------------------------------------------------
 rt_folder <- here("submissions", "rt-forecasts", target_date)
-
 check_dir(rt_folder)
-fwrite(file.path(rt_folder, paste0(target_date, "-epiforecasts-EpiNow2.csv")))
+fwrite(forecast,
+       file.path(rt_folder, paste0(target_date, "-epiforecasts-EpiNow2.csv")))
