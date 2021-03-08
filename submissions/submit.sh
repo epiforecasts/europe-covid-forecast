394cf3ec88d552a9bccff84395467c84aba1befc
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-08 23:06:50 +0000

    revert to both forecasts

--- submissions/submit.sh
+++ submissions/submit.sh
@@ -24,15 +24,18 @@ git merge -Xtheirs main
 cd ../europe-covid-forecast
 
 # Copy your forecast from local folder to submission folder
+cp -R -f "./submissions/rt-forecasts/$ForecastDate/." \
+      "../covid19-forecast-hub-europe/data-processed/epiforecasts-EpiNow2/"
 cp -R -f "./submissions/crowd-forecasts/$ForecastDate/." \
       "../covid19-forecast-hub-europe/data-processed/epiforecasts-EpiExpert/"
+      
 # Commit submission to branch
 cd ../covid19-forecast-hub-europe
 git add --all
 git commit -m "submission"
 
 # Create PR
-gh pr create --title "$ForecastDate - EpiForecast EpiExpert submission" --body " This is an automated submission. Hope your day has been sunshine and rainbows."
+gh pr create --title "$ForecastDate - EpiForecast submission" --body " This is an automated submission. Hope your day has been sunshine and rainbows."
 
 # Remove local submission branch 
 git checkout main
