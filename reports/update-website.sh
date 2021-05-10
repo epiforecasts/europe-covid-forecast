417a7f9a7a30146ee27ab0cdda5213fc7de312f7
nikosbosse <nikosbosse@gmail.com>
2021-05-10 15:14:27 +0200

    delete unnecessary files

--- reports/update-website.sh
+++ reports/update-website.sh
@@ -2,6 +2,7 @@
 
 Rscript reports/compile-ensemble-report.R    
 Rscript reports/compile-evaluation-report.R
+Rscript reports/update-index-file.R
 
 cd ..
 
--- reports/update.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-# update the Crowd forecast report
-Rscript reports/compile-ensemble-report.R
-
-Rscript reports/compile-evaluation-report.R
-
-Rscript reports/update_index_file.R
