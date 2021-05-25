fe3158908513d4555f509c3a727b01ef2bc1459b
Sam Abbott <s.e.abbott12@gmail.com>
2021-05-25 09:39:22 +0000

    drop problem places

--- submissions/submit.sh
+++ submissions/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #define date
-ForecastDate=$(date +'%Y-%m-%d')
+ForecastDate=$(date +'%Y-%m-%d' -d "yesterday")
 
 # Clone the hub repository if not already present
 #git clone --depth 1 https://github.com/epiforecasts/covid19-forecast-hub-europe
@@ -26,20 +26,14 @@ cd ../europe-covid-forecast
 # Copy your forecast from local folder to submission folder
 cp -R -f "./submissions/rt-forecasts/$ForecastDate/." \
       "../covid19-forecast-hub-europe/data-processed/epiforecasts-EpiNow2/"
-cp -R -f "./submissions/crowd-rt-forecasts/$ForecastDate/." \
-      "../covid19-forecast-hub-europe/data-processed/epiforecasts-EpiExpert_Rt/"
-cp -R -f "./submissions/crowd-direct-forecasts/$ForecastDate/." \
-      "../covid19-forecast-hub-europe/data-processed/epiforecasts-EpiExpert_direct/"
-cp -R -f "./submissions/crowd-forecasts/$ForecastDate/." \
-      "../covid19-forecast-hub-europe/data-processed/epiforecasts-EpiExpert/"
-      
+ 
 # Commit submission to branch
 cd ../covid19-forecast-hub-europe
 git add --all
 git commit -m "submission"
 
 # Create PR
-gh pr create --title "$ForecastDate - EpiForecast submission" --body " This is an automated submission. Hope your day has been sunshine and rainbows."
+gh pr create --title "$ForecastDate - EpiForecast submission" --body " This is an automated submission."
 
 # Remove local submission branch 
 git checkout main
