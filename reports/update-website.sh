d1464c4b3e36dd9d7c6b4904e87bfd828792a69b
nikosbosse <nikosbosse@gmail.com>
2021-05-30 14:49:16 +0200

    create report for UK challenge

--- reports/update-website.sh
+++ reports/update-website.sh
@@ -2,8 +2,11 @@
 
 Rscript reports/compile-ensemble-report.R    
 Rscript reports/compile-evaluation-report.R
-Rscript reports/update-index-file.R
+Rscript reports/compile-uk-challenge-report.R
+script reports/update-index-file.R
 
+
+# copy files over to crowd-evaluation repo and push
 cd ..
 
 cp -R -f "europe-covid-forecast/docs/index.html" "crowd-evaluation/"
@@ -23,3 +26,17 @@ git push
 cd ..
 
 cd europe-covid-forecast
+
+# copy files over to uk-challenge repo and push
+
+cd ..
+
+cp "europe-covid-forecast/docs/reports/uk-challenge/index.html" "uk-challenge"
+
+cd uk-challenge
+git add .
+git commit -m "automated update - evaluation report"
+git pull
+git push
+cd ..
+
