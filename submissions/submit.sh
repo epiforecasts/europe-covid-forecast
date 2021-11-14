7dfadae1ed95f00f70c3a9718ae251bcdba77438
Sam Abbott <s.e.abbott12@gmail.com>
2021-11-14 09:52:01 +0000

    automated crowd data update

--- submissions/submit.sh
+++ submissions/submit.sh
@@ -17,7 +17,7 @@ cd ../covid19-forecast-hub-europe
 git checkout main
 git pull 
 # Switch to submission branch
-git checkout -b submission
+git checkout -b submission1
 git merge -Xtheirs main
 
 # Move back into forecast repository
@@ -43,5 +43,5 @@ gh pr create --title "$ForecastDate - EpiForecast submission" --body " This is a
 
 # Remove local submission branch 
 git checkout main
-git branch -D submission
+git branch -D submission1
 cd ../europe-covid-forecast
