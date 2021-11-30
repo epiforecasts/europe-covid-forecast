3668a4d039e246af4648e9228a268da01c8861fb
Hugo Gruson <hugo.gruson@protonmail.com>
2021-11-30 14:43:36 +0100

    Get main.sh working on any week day

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-TODAY=$(date +'%Y-%m-%d')
+if [ $(date +'%A') = 'Monday']; then
+  LAST_MONDAY=$(date +'%Y-%m-%d')
+else
+  LAST_MONDAY=$(date --date='last Mon' +'%Y-%m-%d')
+fi
 mkdir -p data-processed/epiforecasts-EpiNow2
-wget -P data-processed/epiforecasts-EpiNow2 https://raw.githubusercontent.com/epiforecasts/europe-covid-forecast/master/submissions/rt-forecasts/$TODAY/$TODAY-epiforecasts-EpiNow2.csv
+wget -P data-processed/epiforecasts-EpiNow2 https://raw.githubusercontent.com/epiforecasts/europe-covid-forecast/master/submissions/rt-forecasts/$LAST_MONDAY/$LAST_MONDAY-epiforecasts-EpiNow2.csv
